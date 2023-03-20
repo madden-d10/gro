dee776d1e03df05008d6b828fef6bf8354a8eb13
DarraghMadden <madden-d10@ulster.ac.uk>
2023-03-20 11:56:45 +0000

    adds basic form validation

--- frontend/src/components/NewPlantForm.js
+++ frontend/src/components/NewPlantForm.js
@@ -3,7 +3,7 @@ import "../styles/newPlantForm.css"
 
 const formFields = ["name", "group", "lifecycle", "flowerTime", "sunRequirements","uses", "looks", 
 	"color", "plantHeight", "inflorescenceHeight", "wildlifeAttractant", "suitableLocations", "plantHabit",
-	"leaves", "resistances", "soilpH", "spread", "parentage", "childPlants", "fruit", "miscellaneous"]
+	"leaves", "resistances", "soil ph", "spread", "parentage", "childPlants", "fruit", "miscellaneous"]
 
 function NewPlantForm() {
   const createNewPlant = (newPlantObj, formData) => {
@@ -25,8 +25,34 @@ function NewPlantForm() {
 			.then(response => response.json())
   }
 
+	const checkFormValdity = () => {
+		for (const field of formFields) {
+			if ((field === "name" && document.forms[0]["name"].value === "") || (field === "group" && document.forms[0]["group"].value === "")) {
+					document.querySelector(`#${field}`).classList.add('error')
+					alert("Required fields: name and group")
+					return false
+				}
+				
+				document.querySelector(`#${field}`).classList.remove('error')
+			}
+			
+			if (!document.forms[0]["image"].value.toLowerCase().endsWith(".png") && !document.forms[0]["image"].value.toLowerCase().endsWith(".jpg")) {
+				document.querySelector("#image").classList.add('error')
+				alert("Invalid image format")
+				return false
+			}
+			
+			document.querySelector("#image").classList.remove('error')
+			return true
+	}
+
 	const handleSubmit = (event) => {
 		event.preventDefault();
+		const isFormValid = checkFormValdity();
+
+		if (!isFormValid) {
+			return
+		}
 
 		const newPlantObj = {}
 		for (const field of formFields) {
@@ -49,9 +75,17 @@ function NewPlantForm() {
 			const result = field.replace(/([A-Z])/g, " $1");
 			const label = result.charAt(0).toUpperCase() + result.slice(1);
 			
+			if (field === "name" || field === "group") {
+				return (
+					<label key={index} htmlFor={field}>{label}*<br/>
+						<input id={field} name={field} type="text" />
+					</label>
+				)
+			}
+		
 			return (
-				<label key={index} htmlFor={field}>{label}:
-					<input id={field} name={field} type="text"></input>
+				<label key={index} htmlFor={field}>{label}<br/>
+					<input id={field} name={field} type="text" />
 				</label>
 			)
 		})
@@ -59,11 +93,13 @@ function NewPlantForm() {
 
 	return (
 		<div className="form-container">
-		<h2>New Plant</h2>
+			<h2>New Plant</h2>
 			<form className="new-plant-form" onSubmit={handleSubmit}>
 				{renderFormFields()}
-				<input type="file" id="image" name="image" />
-				<input type="submit" value="Submit" className="button"></input>
+				<label htmlFor="image">Plant Image*<br/>
+					<input type="file" id="image" name="image" accept="image/png, image/jpeg" />
+				</label>	
+				<input type="submit" value="Submit" className="button" />
 			</form> 
 		</div>
 		)
